36049ebc489aee05f5c2fecd1c10d3d315d9d071
Mohammed Yasser <mohammedre4a@gmail.com>
2021-08-31 19:13:13 +0200

    refactor: add unified scaffold class. Add: use unifiedScaffold in loginScreen.

--- lib/auth/loginsreen.dart
+++ lib/auth/loginsreen.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:chat_now/Background.dart';
 import 'package:chat_now/auth/registrationscreen.dart';
 import 'package:chat_now/database/databasehelper.dart';
 import 'package:chat_now/home/homescreen.dart';
@@ -10,155 +11,180 @@ import 'package:provider/provider.dart';
 import '../appprovider.dart';
 
 class LoginScreen extends StatefulWidget {
-  /// route name for the login screen
-  static const String routeName='login';
+  static const String routeName = 'login';
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  late AppProvider provider; // late initilization
-  final _loginformkey=GlobalKey <FormState>();
-  String email='';
+  late AppProvider provider;
+  final _loginformkey = GlobalKey<FormState>();
+  String email = '';
 
-  String password='';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    provider= Provider.of<AppProvider>(context);
-    return Stack(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          child:Image.asset('assets/background.png',
-            fit:BoxFit.cover,
-            width:double.infinity,
-            height:double.infinity,
-          ) ,
-        ),
-        Scaffold(
-          backgroundColor:Colors.transparent,
-          appBar: AppBar(
-            title: Text('Login'),
-            centerTitle: true,
-            backgroundColor:Colors.transparent,
-            elevation:0.0,
-          ),
-          // ignore: avoid_unnecessary_containers
-          body:Container(
-              padding: EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                // ignore: prefer_const_literals_to_create_immutables
-                children:[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8.0, 200.0, 8.0, 0),
-                    child: Form(
-                      key: _loginformkey,
-                      child:Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          TextFormField(
-                            onChanged: (textValue){
-                              email=textValue;
-                            },
-                            decoration: InputDecoration(
-                                labelText:'Email',
-                                floatingLabelBehavior:FloatingLabelBehavior.auto
-                            ),
-                            validator: (value){
-                              if(value==null || value.isEmpty){
-                                return 'Please Enter Username';
-                              }
-                              return null;
-                            },
-                          ),
-
-                          TextFormField(
-                            onChanged: (textValue){
-                              password=textValue;
-                            },
-                            decoration: InputDecoration(
-                                labelText:'Password',
-                                floatingLabelBehavior:FloatingLabelBehavior.auto
-                            ),
-                            validator: (value){
-                              if(value==null || value.isEmpty){
-                                return 'Please Enter Password';
-                              }
-                              else if (value.length<6){
-                                return 'Password lenght should be atleat 6 charcaters';
-                              }
-                              return null;
-                            },
-                          ),
-
-                        ],
+    provider = Provider.of<AppProvider>(context);
+    return UnifiedScaffold(
+      title: 'Login',
+      isImplyLeading: false,
+      body: Container(
+          padding: EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Text(
+                'Welcome back!',
+                style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 20.0, 8.0, 0),
+                child: Form(
+                  key: _loginformkey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        onChanged: (textValue) {
+                          email = textValue;
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Email',
+                            floatingLabelBehavior: FloatingLabelBehavior.auto),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Username';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        onChanged: (textValue) {
+                          password = textValue;
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            floatingLabelBehavior: FloatingLabelBehavior.auto),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Password';
+                          } else if (value.length < 6) {
+                            return 'Password length should be at least 6 characters';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                    onPressed: null,
+                    child: Text(
+                      'Forget password?',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
                       ),
                     ),
                   ),
-                  ElevatedButton(
-                      onPressed: (){
-                        if(_loginformkey.currentState?.validate()==true){
-                          createFirebaseUser();
-                        }
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(12),
-                        child:
-                        isloading?Center(
-                          child:CircularProgressIndicator(),
-                        ):Text('Login'),
-                      )),
-                  Spacer(),
-                  TextButton(
-                    onPressed: (){
-                      Navigator.pushNamed(context, RegisterationScreen.routeName);
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_loginformkey.currentState?.validate() == true) {
+                      createFirebaseUser();
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: isloading
+                        ? Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : Text('Login'),
+                  )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(
+                          context, RegisterationScreen.routeName);
                     },
-                    child:Text( 'Create Account!'),
-                  )
-
-                ],) ), )], );
+                    child: Text(
+                      'Or Create My Account!',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )),
+    );
   }
-  bool isloading=false;
-  void createFirebaseUser()async{
+
+  bool isloading = false;
+
+  void createFirebaseUser() async {
     setState(() {
-      isloading=true;
+      isloading = true;
     });
     try {
-      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: email,
-          password: password
-      );
-      if(userCredential.user==null){
-        showErrormessage ('Invalid credientials nou user exist with this email and password');
-      }else{
-        getuserscollection().doc(userCredential.user!.uid).get().then((retriveduser) {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      if (userCredential.user == null) {
+        showErrormessage(
+            'Invalid credientials nou user exist with this email and password');
+      } else {
+        getuserscollection()
+            .doc(userCredential.user!.uid)
+            .get()
+            .then((retriveduser) {
           provider.updateuser(retriveduser.data());
           Navigator.pushReplacementNamed(context, HomeScreen.routeName);
         });
       }
     } on FirebaseAuthException catch (e) {
-      showErrormessage(e.message??'');
-    } catch(e){
+      showErrormessage(e.message ?? '');
+    } catch (e) {
       showErrormessage(e.toString());
     }
     setState(() {
-      isloading=false;
+      isloading = false;
     });
   }
-  void showErrormessage(String message){
+
+  void showErrormessage(String message) {
     showDialog(
         context: context,
-        builder: (buildContext){
-          return AlertDialog(content: Text(message),
+        builder: (buildContext) {
+          return AlertDialog(
+            content: Text(message),
             actions: [
-              TextButton(onPressed: (){
-                Navigator.pop(context);
-              }, child: Text('Ok'))
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Ok'))
             ],
           );
         });
